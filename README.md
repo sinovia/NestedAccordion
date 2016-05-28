1fa6c1a4793c561953cdeed7196987a30552149f
SINOVIA <somayfield@gmail.com>
2016-05-28 19:05:14 -0400

    Functioning Accordion!!!!!!!

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # NestedAccordion
+
+What does it do? The nested accordion make elements of a list visible and invisible by a click.
+
+What interactive elements exist? The section containing the heading of the section is interactive.  
+
+How do you interact with them? By clicking
+
+What visual effects are produced by interacting? The nested list items
+
+How does it do it? A Javascript Event
+
+What existing HTML elements are changed?
+
+What new HTML elements are created?
+
+What CSS styles are used to produce the effect?
---
+++ accordion.js
@@ -0,0 +1,16 @@
+//Oat Cake Button
+document.getElementById("showContent").addEventListener("click", function(){
+    document.getElementsByClassName("dropDownMainhidden")[0].className="dropDownMainshowing"});
+
+
+//Donut Pastry Button
+document.getElementById("showContent2").addEventListener("click", function(){
+    document.getElementsByClassName("dropDownMainhidden2")[0].className="dropDownMainshowing2"});
+
+//Carrot Cake Button
+document.getElementById("showContent3").addEventListener("click", function(){
+    document.getElementsByClassName("dropDownMainhidden3")[0].className="dropDownMainshowing3"});
+
+//Tootsie
+    document.getElementById("showContent4").addEventListener("click", function(){
+        document.getElementsByClassName("dropDownMainhidden4")[0].className="dropDownMainshowing4"});
