0677c1b2fd82409a13c10672ddc55f935177f2cf
SINOVIA <somayfield@gmail.com>
2016-05-31 06:15:12 -0400

    Final Submission Push

--- accordion.js
+++ accordion.js
@@ -1,16 +1,16 @@
-//Oat Cake Button
-document.getElementById("showContent").addEventListener("click", function(){
+//Oat Cake Button Drop
+document.getElementById("dropItLikeItsHot").addEventListener("click", function(){
     document.getElementsByClassName("dropDownMainhidden")[0].className="dropDownMainshowing"});
 
 
-//Donut Pastry Button
-document.getElementById("showContent2").addEventListener("click", function(){
+  //Donut Pastry Button
+document.getElementById("dropItLikeItsHot2").addEventListener("click", function(){
     document.getElementsByClassName("dropDownMainhidden2")[0].className="dropDownMainshowing2"});
 
 //Carrot Cake Button
-document.getElementById("showContent3").addEventListener("click", function(){
+document.getElementById("dropItLikeItsHot3").addEventListener("click", function(){
     document.getElementsByClassName("dropDownMainhidden3")[0].className="dropDownMainshowing3"});
 
 //Tootsie
-    document.getElementById("showContent4").addEventListener("click", function(){
+    document.getElementById("dropItLikeItsHot4").addEventListener("click", function(){
         document.getElementsByClassName("dropDownMainhidden4")[0].className="dropDownMainshowing4"});
